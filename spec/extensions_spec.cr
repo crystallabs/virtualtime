3b3004e56a3255181b0a510908260f1c9f6a2c6b
Davor Ocelic <docelic@spinlocksolutions.com>
2017-06-25 10:13:06 +0200

    Add Array#expand test

---
+++ spec/extensions_spec.cr
@@ -0,0 +1,20 @@
+require "./spec_helper"
+
+describe Crystime do
+
+  it "can expand arrays" do
+    [1, 2, [:a, :b], 8..10, [:e, :f]].expand==
+      [[1, 2, :a, 8, :e],
+       [1, 2, :a, 8, :f],
+       [1, 2, :a, 9, :e],
+       [1, 2, :a, 9, :f],
+       [1, 2, :a, 10, :e],
+       [1, 2, :a, 10, :f],
+       [1, 2, :b, 8, :e],
+       [1, 2, :b, 8, :f],
+       [1, 2, :b, 9, :e],
+       [1, 2, :b, 9, :f],
+       [1, 2, :b, 10, :e],
+       [1, 2, :b, 10, :f]]
+  end
+end
