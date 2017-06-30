68a0c5bb45205f8cf240dde146960d1cd936a06e
Davor Ocelic <docelic@spinlocksolutions.com>
2017-06-30 10:00:39 +0200

    Add on? method to test if item is on

--- src/crystime/item.cr
+++ src/crystime/item.cr
@@ -38,5 +38,129 @@ module Crystime
     #def minute() Int32.new((ssm% 3600)/ 60) end
     #def second() ssm% 60 end
 
+    # Main functions intended for public use
+
+		# TODO: non-interruptible/non-shareable tasks (just a flag)
+		# all existing items should be in @omit when checking if term is free
+
+    # Check whether the item is "on" on the specified date/time. Item is
+    # considered "on" if it matches at least one "due" time and does not
+    # match any "omit" time.
+    # Return values are:
+    # true - item is "on" (it is "due" and not on "omit" list)
+    # false - item is due, but is also omitted, and no reschedule was requested or possible
+    # Time::Span - increments by which "due" should be adjusted until suitable rescheduled date/time is found
+    # nil - not applicable/ not scheduled
+    def on?( date= VirtualDate.now, max_before= nil, max_after= max_before, max_shifts= 1000)
+      d= date
+      return unless d
+      if d.ts.any?{ |x| x== false}
+        raise Crystime::Errors.virtual_comparison
+      end
+      yes= due_on? d
+      no=  omit_on? d
+      #puts self.inspect
+      #puts d.inspect
+      #puts "#{yes}/#{no}"
+      if yes
+        if !no
+          true
+        else # check for shifting due to @omit:
+          #puts "Going into omit search"
+          omit_shift= @omit_shift
+          if omit_shift.is_a? Nil | Bool
+            omit_shift
+          elsif omit_shift.total_milliseconds== 0
+            #p "No shift"
+            false
+          else # Here, omit_shift is virtualdate
+            #puts "Some shift from #{d}"
+            # -1=>search into the future, +1=>search into the past
+            od= d.dup
+            od.ts= d.ts.dup
+            #puts od.class
+            #return true
+            return if !od
+            span= omit_shift
+            #p span.inspect
+            # Counter of times we've tried shifting.
+            shifts= 0
+            ret= loop do
+              #p :in
+              shifts+= 1
+              #p od.inspect
+              #p span.inspect
+              od+= span
+              #p "AFTER +: "+ od.inspect
+              if (max_before&& ((od-d).total_milliseconds.abs> max_before.total_milliseconds)) ||
+               (max_after&& ((od-d).total_milliseconds.abs> max_after.total_milliseconds)) ||
+                (max_shifts&& (shifts> max_shifts))
+                #puts shifts
+								#puts od.inspect
+                #puts @shift.inspect
+                break false
+              end
+              if omit_on? od
+                #puts "This AFTER date is omitted"
+                next
+              elsif due_on? od, @shift
+                #puts od.inspect
+                break true
+              end
+              #puts "didn't match: "+ od.inspect
+              #puts "to: "+ @shift.inspect
+            end
+            #puts "d: #{d.inspect}, od: #{od.inspect}"
+            #puts (od-d).inspect
+            #puts ret.inspect
+            #puts od.class
+            #puts d.class
+            #puts od.ticks
+            #puts d.ticks
+            #puts od.ticks- d.ticks
+            #puts od.inspect
+            #puts od.ticks
+            #a= Time.new( od.ticks)
+            #b= Time.new( d.ticks)
+            #puts a.inspect
+            #puts b.inspect
+            return ret ? (od-d) : ret
+          end
+        end
+      end
+    end
+
+    # Helpers below
+
+#   def parse_timeunit( str)
+#     s= str.upcase
+#     case s
+#     when "S" then 0
+#     when "M" then 60
+#     when "H" then 3600
+#     when "D" then 3600*24
+#     when "W" then 3600*24*7
+#     else raise ArgumentError.new "Unknown time unit: #{s}"
+#     end
+#   end
+#
+#   def time=( time)
+#      @time_ssm= nil
+#      case time
+#        when Array
+#          time[0]* 3600+ time[1]* 60+ time[2]
+#        when Time
+#          time.hour* 3600+ time.minute* 60+ time.second
+#        else nil
+#      end
+#    end
+
+    private def force_array( arg)
+      if !arg.is_a? Array
+        return [arg]
+      else
+        return arg
+      end
+    end
   end
 end
