1b81da30abac162f04479e300ffaac0c1b0f4292
Davor Ocelic <docelic@spinlocksolutions.com>
2018-03-01 11:01:10 +0100

    Minor

--- examples/virtual_date_from_string.cr
+++ examples/virtual_date_from_string.cr
@@ -3,6 +3,6 @@ require "../src/crystime"
 vd = Crystime::VirtualDate["JAN 2018"]
 p vd.month == 1
 
-vd = Crystime::VirtualDate["2018 SUN"]
+vd = Crystime::VirtualDate["2018 sun"]
 p vd.weekday == 0
 
