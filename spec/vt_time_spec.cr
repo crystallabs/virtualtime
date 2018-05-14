a24d2d8c5c3862cb44b40e75dda21827433f9024
Davor Ocelic <docelic@crystallabs.io>
2018-05-15 00:55:06 +0200

    Add more tests and convenience methods

--- spec/vt_time_spec.cr
+++ spec/vt_time_spec.cr
@@ -4,15 +4,23 @@ require "./spec_helper"
 
 describe "Crystime::VirtualTime and Times" do
 
-	it "can match Crystal's Time structs" do
-		vt= Crystime::VirtualTime.new
+  it "can match Crystal's Time structs" do
+    vt= Crystime::VirtualTime.new
 
-		Crystime::Helpers.matches?( vt, [Time.now]).should be_true
+    Crystime::Helpers.matches?( vt, [Time.now]).should be_true
+    vt.matches?( [Time.now]).should be_true
+    Crystime::VirtualTime.now.matches?(vt).should be_true
 
-		vt.month= 3
-		vt.day = (10..20).step(2)
+    vt.month= 3
+    vt.day = (10..20).step(2)
 
-		Crystime::Helpers.matches?( vt, [Time.parse("2018-03-10", "%F")]).should be_true
-		Crystime::Helpers.matches?( vt, [Time.parse("2018-03-11", "%F")]).should be_nil
-	end
+    Crystime::Helpers.matches?( vt, [Time.parse("2018-03-10", "%F")]).should be_true
+    Crystime::Helpers.matches?( vt, [Time.parse("2018-03-11", "%F")]).should be_nil
+    # Same thing as above but in alternate notation:
+    vt.matches?( [Time.parse("2018-03-10", "%F")]).should be_true
+    vt.matches?( [Time.parse("2018-03-11", "%F")]).should be_nil
+    # Same thing as above but in alternate notation:
+    Crystime::VirtualTime["2018-03-10"].matches?(vt).should be_true
+    Crystime::VirtualTime["2018-03-11"].matches?(vt).should be_nil
+  end
 end
